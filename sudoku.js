22dd3b995108566b7e1c4bd7b934d5690efe2a19
Angelina26 <gagelganc00@mail.ru>
2024-01-12 13:39:57 +0300

    transpose

--- sudoku.js
+++ sudoku.js
@@ -25,3 +25,13 @@ function prettyBoard() {
    * Подумай, как симпатичнее его вывести.
    */
 }
+
+function transpose(transpose) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+}
